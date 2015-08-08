e62d1fb849679d602fe2ee143a34074160ffa2c6
Amy Burka <aburka@gmail.com>
2015-08-08 18:08:05 -0400

    Add static data to route

--- app/routes/tattoo/index.js
+++ app/routes/tattoo/index.js
@@ -1,4 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(){
+    return [
+      {
+        id: 1,
+        src: 'images/sparrow.jpg',
+        w: 400,
+        h: 400,
+        caption: "what a beautiful sparrow",
+        tags: ["put a bird on it", "color"]
+      },
+      {
+        id: 2,
+        src: 'images/star_sparrow.jpg',
+        w: 400,
+        h: 400,
+        caption: "sparrow with stars near its butt",
+        tags: ["put a bird on it", "color", "stars"]
+      },
+      {
+        id: 3,
+        src: 'images/fork.jpg',
+        w: 400,
+        h: 400,
+        caption: "fork!",
+        tags: ["black & white", "dotwork"]
+      }
+    ];
+  }
 });
