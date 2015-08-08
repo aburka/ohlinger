fd31a10a227753ae200399ea223242f80baf8bb3
Amy Burka <aburka@gmail.com>
2015-08-08 15:19:50 -0400

    Delete Brocfile, Add routes for index, tattoo index, art index, store, about and contact

--- app/router.js
+++ app/router.js
@@ -6,6 +6,11 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('tattoo', function() {});
+  this.route('art', function() {});
+  this.route('about');
+  this.route('contact');
+  this.route('store');
 });
 
 export default Router;
