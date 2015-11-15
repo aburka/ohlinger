f1c249c53deb73eb5855114a15a8ca7c5381d4a5
Amy Burka <aburka@gmail.com>
2015-11-15 17:40:22 -0500

    Remove unnecessary model

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,13 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  actions: {
-    // delete: function(artwork){
-    //   artwork.destroyRecord();
-    // },
-    //
-    // edit: function(artwork){
-    //   artwork.save();
-    // },
-  }
+
 });
