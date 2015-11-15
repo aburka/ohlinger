48618a1df47d86cf267a0ff8c5bcbfa5fd076e20
Jacob Smith <jacob@jacobsmith.io>
2015-11-15 15:45:01 -0500

    Implement fetching with Parse JS SDK

--- app/routes/tattoo/index.js
+++ app/routes/tattoo/index.js
@@ -2,5 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(){
+    var query = new Parse.Query('Image');
+    return query.find().then(function(results) {
+      return _.invoke(results, 'toJSON');
+    });
   }
 });
