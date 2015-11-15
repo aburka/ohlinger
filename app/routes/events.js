db3f33cc1c280adfbfbc71ecd88c53dd10f31302
Amy Burka <aburka@gmail.com>
2015-11-15 16:13:48 -0500

    Show events from Parse

--- app/routes/events.js
+++ app/routes/events.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(){
-    return this.store.findAll('event');
+    var query = new Parse.Query('Event');
+    return query.find().then(function(results) {
+      return _.invoke(results, 'toJSON');
+    });
   }
 });
