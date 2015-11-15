ec71bea7b81a9489442ee92038b87474b0620904
Amy Burka <aburka@gmail.com>
2015-11-15 17:36:46 -0500

    move tag query to tags/show route

--- app/routes/tattoo/tags/show.js
+++ app/routes/tattoo/tags/show.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(params){
+    var query = new Parse.Query('Image');
+    query.equalTo("tags", params);
+    return query.find().then(function(results) {
+      console.log();
+      return _.invoke(results, 'toJSON');
+    });
+  }
 });
