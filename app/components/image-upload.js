e23618fe9e195502720cc0d61698e70262a53eb6
Jacob Smith <jacob@jacobsmith.io>
2015-11-15 16:38:56 -0500

    Implement image upload

--- app/components/image-upload.js
+++ app/components/image-upload.js
@@ -1,5 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'form'
+  tagName: 'form',
+  submit(e) {
+    e.preventDefault();
+    var imageInput = this.$('input[type=file]')[0];
+    if(imageInput.files.length > 0) {
+      var image = new Parse.Object('Image');
+      image.set(this.getProperties('name', 'caption'));
+      var file = imageInput.files[0];
+      var imageFile = new Parse.File(image.name, file);
+      imageFile.save().then(() => {
+        image.set('image', imageFile);
+        return image.save();
+      }, (error) => {
+        console.error(error);
+      });
+    }
+  }
 });
