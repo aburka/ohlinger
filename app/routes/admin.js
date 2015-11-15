a29b75ee8ea8a8b0a923f8a136340b5a1319e151
Jacob Smith <jacob@jacobsmith.io>
2015-11-15 16:24:29 -0500

    Extract image-upload to component

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,22 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  actions: {
-    // delete: function(artwork){
-    //   artwork.destroyRecord();
-    // },
-    //
-    // edit: function(artwork){
-    //   artwork.save();
-    // },
-
-    filepick: function(blob){
-      console.log(blob, "blob");
-      var upload = this.store.createRecord('image', {
-        url : blob.url,
-        filename : blob.filename,
-      });
-      upload.save(blob);
-    }
-  }
 });
