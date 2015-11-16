915cf3e8c17e261a24683dc8fa5d778530fb4318
Amy Burka <aburka@gmail.com>
2015-11-16 13:21:28 -0500

    Set up Parse queries for art routes

--- app/routes/art/artistic-identity.js
+++ app/routes/art/artistic-identity.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(){
+    var query = new Parse.Query('Art');
+    query.equalTo("category", "Artistic Identity");
+    return query.find().then(function(results) {
+      return _.invoke(results, 'toJSON');
+    });
+  }
 });
--- app/routes/art/subway-paintings.js
+++ app/routes/art/subway-paintings.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(){
+    var query = new Parse.Query('Art');
+    query.equalTo("category", "Subway Paintings");
+    return query.find().then(function(results) {
+      return _.invoke(results, 'toJSON');
+    });
+  }
 });
