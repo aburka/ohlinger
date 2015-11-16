41c41ba07693d265e83a9861fe69582dba90e190
Jacob Smith <jacob@jacobsmith.io>
2015-11-16 13:58:22 -0500

    Fix tag query
    1. Because route parameter is `:tag`, it must be accessed as
    `params.tag`
    2. Fix name mismatch in template
    3. Hook up model in template

--- app/routes/tattoo/tags/show.js
+++ app/routes/tattoo/tags/show.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params){
     var query = new Parse.Query('Image');
-    query.equalTo("tags", params);
+    query.containsAll("tags", [params.tag]);
     return query.find().then(function(results) {
       return _.invoke(results, 'toJSON');
     });
