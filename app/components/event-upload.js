0d113cd928783b70f9cb757f074aa66eeff6f735
Amy Burka <aburka@gmail.com>
2015-11-16 00:14:48 -0500

    move event upload to component

---
+++ app/components/event-upload.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'form',
+  submit(e) {
+    e.preventDefault();
+    var imageInput = this.$('input[type=file]')[0];
+    if(imageInput.files.length > 0) {
+      var image = new Parse.Object('Event');
+      image.set(this.getProperties('title', 'start_date', 'end_date', 'description'));
+      var file = imageInput.files[0];
+      var imageFile = new Parse.File(image.name, file);
+      imageFile.save().then(() => {
+        image.set('image', imageFile);
+        return image.save();
+      }, (error) => {
+        console.error(error);
+      });
+    }
+  }
+});
