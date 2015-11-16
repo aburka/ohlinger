2e07ee00d8631aac7aeb12af6eab16667495841f
Jacob Smith <jacob@jacobsmith.io>
2015-11-16 14:18:12 -0500

    Remove ember-data files

--- app/models/event.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  title: DS.attr('string'),
-  start_date: DS.attr('date'),
-  end_date: DS.attr('date'),
-  description: DS.attr('string'),
-  image: DS.attr('file')
-});
