9ba0a479dcbca4b0f967f22bfcb2326eadf99fe4
Amy Burka <aburka@gmail.com>
2015-11-16 22:42:08 -0500

    Try to get mandrill working, not quite there

--- app/routes/about/contact.js
+++ app/routes/about/contact.js
@@ -1,31 +1,31 @@
 import Ember from 'ember';
+import $ from 'jquery';
 
 export default Ember.Route.extend({
-  actions:{
-    submitContact: function(params){
-      $.ajax({
-        type: 'POST',
-        url: 'https://mandrillapp.com/api/1.0/messages/send.json',
-        data: {
-          'key': 'jXxFN11eNF57YzQIIocMzg',
-          'message': {
-            'from_email': params.email,
-            'from_name': params.name,
-            "text": params.comments,
-            'to': [
-                {
-                  'email': 'amyburka@gmail.com',
-                  'name': 'Amy',
-                  'type': 'to'
-                },
-              ],
-            'autotext': 'true',
-            'subject': 'Ohlinger.com Appointment Inquiry',
-          }
+  submit(e, params){
+    e.preventDefault();
+    $.ajax({
+      type: 'POST',
+      url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+      data: {
+        'key': 'jXxFN11eNF57YzQIIocMzg',
+        'message': {
+          'from_email': params.email,
+          'from_name': params.name,
+          "text": params.comments,
+          'to': [
+              {
+                'email': 'amyburka@gmail.com',
+                'name': 'Amy',
+                'type': 'to'
+              },
+            ],
+          'autotext': 'true',
+          'subject': 'Ohlinger.com Appointment Inquiry',
         }
-       }).done(function(response) {
-         console.log(response);
-       });
-    }
+      }
+     }).done(function(response) {
+       console.log(response);
+     });
   }
 });
