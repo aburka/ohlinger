f376b6887667e2b6bebb14d3867214d721984961
Amy Burka <aburka@gmail.com>
2015-11-19 08:27:41 -0500

    Move submit contact actions to component

--- app/components/contact-appt.js
+++ app/components/contact-appt.js
@@ -1,13 +1,34 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-    submitContact: function(){
-      this.sendAction('submitContact', this.getProperties('name', 'email', 'phone', 'comments'));
-      this.set('name');
-      this.set('email');
-      this.set('phone');
-      this.set('comments');
-    }
-  },
+  submit(e, params){
+    console.log("hello");
+    e.preventDefault();
+    $.ajax({
+      type: 'POST',
+      url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+      data: {
+        'key': 'jXxFN11eNF57YzQIIocMzg',
+        'message': {
+          'from_email': params.email,
+          'from_name': params.name,
+          "text": params.comments,
+          'to': [
+              {
+                'email': 'amyburka@gmail.com',
+                'name': 'Amy',
+                'type': 'to'
+              },
+            ],
+          'autotext': 'true',
+          'subject': 'Ohlinger.com Appointment Inquiry',
+        },
+        "headers": {
+            "Reply-To": params.email
+        }
+      }
+     }).done(function(response) {
+       console.log(response);
+     });
+  }
 });
--- app/routes/about/contact.js
+++ app/routes/about/contact.js
@@ -2,30 +2,4 @@ import Ember from 'ember';
 import $ from 'jquery';
 
 export default Ember.Route.extend({
-  submit(e, params){
-    e.preventDefault();
-    $.ajax({
-      type: 'POST',
-      url: 'https://mandrillapp.com/api/1.0/messages/send.json',
-      data: {
-        'key': 'jXxFN11eNF57YzQIIocMzg',
-        'message': {
-          'from_email': params.email,
-          'from_name': params.name,
-          "text": params.comments,
-          'to': [
-              {
-                'email': 'amyburka@gmail.com',
-                'name': 'Amy',
-                'type': 'to'
-              },
-            ],
-          'autotext': 'true',
-          'subject': 'Ohlinger.com Appointment Inquiry',
-        }
-      }
-     }).done(function(response) {
-       console.log(response);
-     });
-  }
 });
