4365cb690edf1438611b4e63ff957a175743b3c9
Amy Burka <aburka@gmail.com>
2015-11-20 08:16:41 -0500

    1. Add 'event-items' component to wrap around 'event-item' 2. Add show/hide actions for the edit form

--- app/components/event-item.js
+++ app/components/event-item.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ["col-sm-6", "col-md-3"],
+  
+  session: Ember.inject.service('session'),
+
+  isHidden: true,
+
+  actions: {
+    showEdit(){
+      this.set("isHidden", false);
+    },
+    cancelEdit(){
+      this.set("isHidden", true);
+    }
+  }
 });
