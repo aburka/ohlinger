a5420fa860cccc0a4c3effc4aa019aea9e87e120
Amy Burka <aburka@gmail.com>
2015-11-20 08:19:36 -0500

    1. Add 'store-item' component to handle actions on each item in the store. 2. Add show/hide actions to edit form

---
+++ app/components/store-item.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['col-sm-6', 'col-md-4'],
+  session: Ember.inject.service('session'),
+
+  isHidden: true,
+
+  actions: {
+    showEdit(){
+      this.set("isHidden", false);
+    },
+    cancelEdit(){
+      this.set("isHidden", true);
+    }
+  }
+});
