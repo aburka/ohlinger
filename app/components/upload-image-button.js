fbbf39c77f7e5ed79c6ca6b4b995dded49922cd4
Jacob Smith <jacob@jacobsmith.io>
2015-11-24 16:07:54 -0500

    Finish upload-image-button

--- app/components/upload-image-button.js
+++ app/components/upload-image-button.js
@@ -4,9 +4,30 @@ export default Ember.Component.extend({
   classNames: ['upload-image-button'],
   isUploading: false,
 
+  input: null,
+  loadInput: function() {
+    this.set('input', this.$('[data-behavior=hidden-file-input]'));
+  }.on('didInsertElement'),
+
   actions: {
+    loadFile() {
+      var input = this.get('input');
+      input.trigger('click');
+    },
+
     uploadImage() {
       this.set('isUploading', true);
+      var input = this.get('input');
+      var file = input[0].files[0];
+      var imageFile = new Parse.File(file.name, file);
+      imageFile.save()
+      .then(() => {
+        this.set('value', imageFile.toJSON());
+        this.sendAction('onUpload', imageFile.toJSON());
+      })
+      .always(() => {
+        this.set('isUploading', false);
+      });
     }
   }
 });
