2b1d77495ca94fc9f736872d7b6b9390d93e450b
Jacob Smith <jacob@jacobsmith.io>
2015-11-23 21:24:30 -0500

    Finish upload button UI

--- app/components/upload-image-button.js
+++ app/components/upload-image-button.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['upload-image-button'],
+  isUploading: false,
 
+  actions: {
+    uploadImage() {
+      this.set('isUploading', true);
+    }
+  }
 });
