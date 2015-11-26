fd5a9652beb599ee5748507da83cb867555c5e08
Jacob Smith <jacob@jacobsmith.io>
2015-11-26 13:14:07 -0500

    Refactor event store

--- app/services/event-store.js
+++ app/services/event-store.js
@@ -2,6 +2,15 @@ import Ember from 'ember';
 
 const twelveAM = moment().startOf('day').toDate();
 const midnight = moment().endOf('day').toDate();
+const past = [];
+const current = [];
+
+function serializeEvent(event) {
+  return _.extend({}, event.toJSON(), {
+    start_date: event.get('start_date'),
+    end_date: event.get('end_date')
+  });
+}
 
 export default Ember.Service.extend({
   past: null,
@@ -9,17 +18,17 @@ export default Ember.Service.extend({
 
   init() {
     this._super(...arguments);
-    this.set('past', []);
-    this.set('current', []);
+    this.set('past', past);
+    this.set('current', current);
   },
 
   fetch() {
-    this.fetchPast();
-    this.fetchCurrent();
+    this._fetchPast();
+    this._fetchCurrent();
   },
 
   // end_date is before 12am today, or end_date doesn't exist and start_date is before 12am today
-  fetchPast() {
+  _fetchPast() {
     var query1 = new Parse.Query('Event');
     query1.lessThan('end_date', twelveAM);
 
@@ -29,19 +38,12 @@ export default Ember.Service.extend({
 
     var query = Parse.Query.or(query1, query2);
     return query.find().then((results) => {
-      let pastEvents = this.get('past');
-      pastEvents.setObjects([]);
-      pastEvents.pushObjects(results.map((r) => {
-        return _.extend({}, r.toJSON(), {
-          start_date: r.get('start_date'),
-          end_date: r.get('end_date')
-        });
-      }));
+      past.setObjects(results.map((r) => serializeEvent(r) ));
     });
   },
 
   // start_date is after 12am today or end_date is after 11:59pm today
-  fetchCurrent() {
+  _fetchCurrent() {
     var query1 = new Parse.Query('Event');
     query1.greaterThanOrEqualTo('start_date', twelveAM);
 
@@ -50,14 +52,7 @@ export default Ember.Service.extend({
 
     var query = Parse.Query.or(query1, query2);
     return query.find().then((results) => {
-      let currentEvents = this.get('current');
-      currentEvents.setObjects([]);
-      currentEvents.pushObjects(results.map((r) => {
-        return _.extend({}, r.toJSON(), {
-          start_date: r.get('start_date'),
-          end_date: r.get('end_date')
-        });
-      }));
+      current.setObjects(results.map((r) => serializeEvent(r) ));
     });
   }
 });
