1f168f5ef9ef238d254a267494d7915ec6025107
Amy Burka <aburka@gmail.com>
2015-12-05 16:55:22 -0500

    Fixes #11

--- app/components/contact-appt.js
+++ app/components/contact-appt.js
@@ -1,34 +1,34 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.Component.extend({
-  submit(e, params){
-    console.log("hello");
-    e.preventDefault();
-    $.ajax({
-      type: 'POST',
-      url: 'https://mandrillapp.com/api/1.0/messages/send.json',
-      data: {
-        'key': 'jXxFN11eNF57YzQIIocMzg',
-        'message': {
-          'from_email': params.email,
-          'from_name': params.name,
-          "text": params.comments,
-          'to': [
-              {
-                'email': 'amyburka@gmail.com',
-                'name': 'Amy',
-                'type': 'to'
-              },
-            ],
-          'autotext': 'true',
-          'subject': 'Ohlinger.com Appointment Inquiry',
-        },
-        "headers": {
-            "Reply-To": params.email
+  actions: {
+    submit(){
+      $.ajax({
+        type: "POST",
+        url: "https://mandrillapp.com/api/1.0/messages/send.json",
+        data: {
+          "key": "jXxFN11eNF57YzQIIocMzg",
+          "message": {
+            "from_email": this.email,
+            "from_name": this.name,
+            "text": this.comments,
+            "to": [
+                {
+                  "email": "jj@ohlinger.com",
+                  "name": "JJ",
+                  "type": "to"
+                },
+              ],
+            "autotext": "true",
+            "subject": "Ohlinger.com Appointment Inquiry",
+          },
+          "headers": {
+              "Reply-To": this.email
+          }
         }
-      }
-     }).done(function(response) {
-       console.log(response);
-     });
+      }).done(function(response) {
+         console.log(response);
+      });
+    }
   }
 });
