37e6b55ccbd857802915e30b50c6c9f3afe8350c
Jacob Smith <jacob@jacobsmith.io>
2015-12-06 12:16:01 -0500

    Refactor tattoo to pass down model

--- app/routes/tattoo/show.js
+++ app/routes/tattoo/show.js
@@ -3,8 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params){
     var query = new Parse.Query('Image');
-    return query.get(params.objectId).then(function(image) {
-      return image.toJSON();
-    });
+    return query.get(params.objectId);
   }
 });
