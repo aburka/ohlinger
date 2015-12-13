32d87deb1bc2d9cc1c6fa5d2b962a689921e6398
Amy Burka <aburka@gmail.com>
2015-12-12 19:16:14 -0500

    Fixes #44

--- app/routes/events.js
+++ app/routes/events.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(){
     var query = new Parse.Query("Event");
-    query.descending();
+    query.addAscending('start_date');
     return query.find();
-  }
+  } 
 });
