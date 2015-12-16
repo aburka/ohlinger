3187a066a12b74d884f556389736b199b137192b
Amy Burka <aburka@gmail.com>
2015-12-16 18:38:03 -0500

    Fixes #50

--- app/routes/tattoo/tags/show.js
+++ app/routes/tattoo/tags/show.js
@@ -4,17 +4,9 @@ export default Ember.Route.extend({
   model: function(params){
     var query = new Parse.Query('Image');
     query.containsAll("tags", [params.tag]);
+    query.addDescending('updatedAt');
     return query.find().then(function(results) {
-      var sortedResults = results.sort(function(a, b) {
-        if (a.updatedAt < b.updatedAt) {
-          return -1;
-        } else if (a.updatedAt === b.updatedAt) {
-          return 0;
-        } else {
-          return 1;
-        }
-      });
-      return _.invoke(sortedResults, 'toJSON');
+      return _.invoke(results, 'toJSON');
     });
   }
 });
