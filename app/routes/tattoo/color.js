cd5e6e6a3fd6cb15cbfbbeecfb37517bf645bfaf
Amy Burka <aburka@gmail.com>
2015-12-16 18:36:26 -0500

    Fixes #49

--- app/routes/tattoo/color.js
+++ app/routes/tattoo/color.js
@@ -4,17 +4,10 @@ export default Ember.Route.extend({
   model: function(){
     var query = new Parse.Query('Image');
     query.equalTo("category", "Color");
+    query.limit(1000);
+    query.addDescending('updatedAt');
     return query.find().then(function(results) {
-      var sortedResults = results.sort(function(a, b) {
-        if (a.updatedAt > b.updatedAt) {
-          return -1;
-        } else if (a.updatedAt === b.updatedAt) {
-          return 0;
-        } else {
-          return 1;
-        }
-      });
-      return _.invoke(sortedResults, 'toJSON');
+      return _.invoke(results, 'toJSON');
     });
   }
 });
