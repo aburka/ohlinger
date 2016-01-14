1c4f9e2245c5665b9e34f741d6a8905c8937348a
Amy Burka <aburka@gmail.com>
2016-01-14 14:21:14 -0500

    Query for image urls

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -2,4 +2,22 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  actions: {
+    downloadImages() {
+      var tattoos = Parse.Object.extend("Image");
+      var query = new Parse.Query(tattoos);
+      query.limit(1000);
+      return query.find().then(function(results) {
+        var imageObject = _.invoke(results, 'toJSON');
+        imageObject.forEach(function(imageObject){
+          var url = imageObject.image.url;
+          console.log(url);
+          // var blob = new Blob([url], {type: "image/jpeg"});
+          // saveAs(blob, "images.zip");
+        });
+      });
+      // var url = image.get("image").url;
+      // console.log(url);
+    }
+  }
 });
